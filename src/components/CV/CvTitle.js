94d2db9ace05db002c26423f05776c1d23332666
Pedro BM <p.bonilla.miralles@gmail.com>
2019-04-08 20:43:35 +0200

    picture sent to cv

--- src/components/CV/CvTitle.js
+++ src/components/CV/CvTitle.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react'
 
 class CvTitle extends Component {
-  state = {
-    isUploaded: false,
-  }
-
-  handleUpload = () => {
-    this.setState({
-      isUploaded: true
-    })
-  }
-
-  
-
   render() {
     const { firstName, lastName } = this.props.user.contact;
+    const { avatarURL } = this.props.user;
+    console.log('avatarURL', avatarURL)
     const { headline, summary } = this.props.cv;
     return (
       <article className="cv-title-article">
-        <img src={require("../../images/logo.png")} alt="profile"/>
+        <img src={avatarURL} alt="profile"/>
         <div className="cv-title-text">
           <h2>{`${firstName} ${lastName}`}</h2>
           <h3>{headline}</h3>
