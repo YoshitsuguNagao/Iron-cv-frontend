8ca50adf793edc8ed1403e0ca3be7c210daa2870
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-04-10 12:49:48 +0200

    commit for change branch

---
+++ src/components/PDF/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+
+class Header extends Component {
+  render() {
+    const { firstName, lastName } = this.props.user.contact;
+    const { avatarURL } = this.props.user;
+    const { headline, summary } = this.props.cv;
+    return (
+      <article className="cv-title-article">
+        <img src={avatarURL} alt="profile"/>
+        <div className="cv-title-text">
+          <h2>{`${firstName} ${lastName}`}</h2>
+          <h3>{headline}</h3>
+          <p>{summary}</p>
+        </div>
+      </article>
+    );
+  }
+};
+
+export default Header;
