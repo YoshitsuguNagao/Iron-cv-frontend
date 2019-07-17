700e73b8dc1bf6851e52ead220ce2ee9a158f0b8
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-17 22:30:41 +0200

    fix cv css

--- src/components/CV/CvWork.js
+++ src/components/CV/CvWork.js
@@ -7,8 +7,10 @@ class CvWork extends Component {
       <div className="work-container">
         <div className="work-title">{content.title}</div>
         <div className="work-company">{content.name}</div>
-        <div className="work-text">{`${content.startDate.month}/${content.startDate.year} ~ ${content.endDate.month}/${content.endDate.year}`}</div>
-        <div className="work-text">{content.city}</div>
+        <div className="date-city">
+          <div className="work-text">{`${content.startDate.month}/${content.startDate.year} ~ ${content.endDate.month}/${content.endDate.year}`}</div>
+          <div className="work-text">{content.city}</div>
+        </div>
         <div className="work-text">{content.description}</div>
         <div className="work-text">{content.tasks}</div>
       </div>
