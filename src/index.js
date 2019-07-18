35abf850ab3bf28fbfef0b00eadc628b1241cfe5
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-18 23:12:00 +0100

    refectoring to scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import './styles/style.scss';
 
 // import 'milligram/dist/milligram.css';
 
